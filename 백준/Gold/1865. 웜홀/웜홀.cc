03d9d493bbaf40ae861569e7237193e97e275d51
YeongJin Jeong <81943525+luminouxx@users.noreply.github.com>
2024-07-08 19:58:22 +0900

    [Gold III] Title: 웜홀, Time: 24 ms, Memory: 2332 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.cc
+++ 백준/Gold/1865. 웜홀/웜홀.cc
@@ -1,61 +1,79 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include<iostream>
+#include<vector>
+#include<queue>
+
+#define MAX_N 501
+#define MAX_DIST 10000000
 
 using namespace std;
 
-vector<pair<pair<int, int>, int>> v;
-int dist[501];
+int TC, N, M, W;
 
-void bellmanFord(int n) {
-    for (int i = 0; i < n; i++) {
-        for (int pos = 0; pos < v.size(); pos++) {
-            int from = v[pos].first.first;
-            int to = v[pos].first.second;
-            int cost = v[pos].second;
+vector<pair<pair<int, int>, int>> edge;
 
-            if (dist[from] + cost < dist[to]) dist[to] = dist[from] + cost;
+bool bellman_ford() {
+    int dist[MAX_N];
+
+    for (int i=1; i<N; i++) {
+        dist[i] = MAX_DIST;
+    }
+
+    for (int j=1; j<N; j++) {
+        for (int k=0; k<edge.size(); k++) {
+            int from = edge[k].first.first;
+            int to = edge[k].first.second;
+            int cost = edge[k].second;
+
+            if (dist[to] > dist[from] + cost) { 
+                dist[to] = dist[from] + cost;
+            }
         }
     }
 
-    for (int pos = 0; pos < v.size(); pos++) {
-        int from = v[pos].first.first;
-        int to = v[pos].first.second;
-        int cost = v[pos].second;
-        if (dist[from] + cost < dist[to]) {
-            cout << "YES\n";
-            return;
+    for (int j=1; j<N; j++) {
+        for (int k=0; k<edge.size(); k++) {
+            int from = edge[k].first.first;
+            int to = edge[k].first.second;
+            int cost = edge[k].second;
+
+            if (dist[to] > dist[from] + cost) { 
+                dist[to] = dist[from] + cost;
+                return true;
+            }
         }
     }
 
-    cout << "NO\n";
-
+    return false;
 }
 
-
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-    int tc; cin >> tc;
+    cin >> TC;
 
-    while (tc--) {
-        v.clear();
-        int n, m, w; cin >> n >> m >> w;
-        for (int i = 1; i <= n; i++) dist[i] = 987654321;
+    for (int i=0; i<TC; i++) {
+        cin >> N >> M >> W;
+        edge.clear();
 
-        for (int i = 0; i < m; i++) {
-            int from, to, cost; cin >> from >> to >> cost;
-            v.push_back({ {from,to},cost });
-            v.push_back({ {to,from},cost });
+        for (int j=0; j<M; j++) {
+            int S, E, T;
+            cin >> S >> E >> T;
+            edge.push_back({{S, E}, T});
+            edge.push_back({{E, S}, T});
         }
-        for (int i = 0; i < w; i++) {
-
-            int from, to, cost; cin >> from >> to >> cost;
-            v.push_back({ {from,to},-cost });
+        for (int k=0; k<W; k++) {
+            int wS, wE, wT;
+            cin >> wS >> wE >> wT;
+            edge.push_back({{wS, wE}, -wT});
         }
-
-        bellmanFord(n);
+        if (bellman_ford()) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
     }
-
 
     return 0;
 }
