2cb2fad0f301913f3491305fcadfb435dac56832
YeongJin Jeong <81943525+luminouxx@users.noreply.github.com>
2024-09-25 12:30:34 +0900

    [level 1] Title: 폰켓몬, Time: 1.56 ms, Memory: 4.32 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+
+#include <iostream>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> nums) {
+    int answer = 0;
+    
+    set<int> s;
+    
+    for (auto num: nums) {
+        s.insert(num);
+    }
+    
+    answer = min(s.size(), nums.size() / 2);
+    
+    return answer;
+}
